bc9f8045d95941cebec8b116c7cb104237fe6424
Test <aovolkov1@gmail.com>
2024-11-25 01:57:31 +0300

    CamelCase

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        int initial_balance = 200;       // Начальный баланс
+        int initialBalance = 200;       // Начальный баланс
         int addition = 1500;       // Сумма пополнения
 
         if (addition > 1000) {
             int bonuses = addition / 100;
-            int final_balance = initial_balance + addition + bonuses;
-            System.out.println(final_balance + " Баланс");
+            int finalBalance = initialBalance + addition + bonuses;
+            System.out.println(finalBalance + " Баланс");
             System.out.println(bonuses + " Бонусные рубли");
         } else {
-            int final_balance = initial_balance + addition;
-            System.out.println(final_balance + " Баланс");
+            int finalBalance = initialBalance + addition;
+            System.out.println(finalBalance + " Баланс");
         }
 
     }
